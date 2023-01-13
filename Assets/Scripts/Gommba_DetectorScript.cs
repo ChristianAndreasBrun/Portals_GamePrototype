b62d82c4f22fc2da3433f3ba115819d6cd861514
OtherAKIRA <andreas.christian.brun@gmail.com>
2023-01-13 23:04:56 +0100

    Enemy develop

---
+++ Assets/Scripts/Gommba_DetectorScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gommba_DetectorScript : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D Rigidbody;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
